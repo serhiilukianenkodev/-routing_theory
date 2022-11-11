25bc5aa1e6bd22ec51e2a3e795ae796e3cb5e17a
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2022-11-11 22:22:08 +0200

    add routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import { Home } from 'pages/Home';
+import { About } from 'pages/About';
+import { Products } from 'pages/Products';
+import { NotFound } from 'pages/NotFound';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Routes path="http://localhost:3000/routing_theory">
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
